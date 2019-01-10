244175024268d68f9d8b6658820f4838a3c77c31
Wellington Gustavo Macedo <wellingtongustavomacedo@gmail.com>
2019-01-10 14:05:40 -0200

    Many tests

--- main.go
+++ main.go
@@ -9,3 +9,23 @@ func main() {
 func sum(n1 int, n2 int) int {
     return n1 + n2;
 }
+
+func multiply(n1 int, n2 int) int {
+    return n1 * n2;
+}
+
+func subtract(n1 int, n2 int) int {
+    return n1 - n2;
+}
+
+func divide(n1 int, n2 int) int {
+    return n1/n2;
+}
+
+func print(message string) {
+    fmt.Println(message);
+}
+
+func print2(message string) {
+    fmt.Println(message);
+}
--- main_test.go
+++ main_test.go
@@ -13,3 +13,21 @@ func TestMain_Sum(t *testing.T) {
 	result := sum(1, 1);
 	assert.Equal(t, 2, result)
 }
+
+func TestMain_Subtract(t *testing.T) {
+	result := subtract(1, 1);
+	assert.Equal(t, 0, result)
+}
+
+func TestMain_Divide(t *testing.T) {
+	result := divide(1, 1);
+	assert.Equal(t, 1, result)
+}
+
+func TestMain_Print(t *testing.T) {
+	print("oi");
+}
+
+func TestMain_Print2(t *testing.T) {
+	print2("oi");
+}
